e1f63ff85c68141edcb4214747f1b7cb976c7ea8
ZeuS <ponzeadrian@gmail.com>
2024-05-02 20:17:12 +1000

    input products for books category

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -125,7 +125,69 @@ db.once('open', async () => {
       category: categories[0]._id,
       price: 999.99,
       qauntity: 100
-    }
+    },
+    {
+      name: 'The Alchemist" by Paulo Coelho',
+      description:
+        "A timeless tale of self-discovery and following one's dreams.",
+      image: 'alchemist.jpg',
+      category: categories[1]._id,
+      price: 19.99,
+      qauntity: 500
+    },
+    {
+      name: 'Becoming" by Michelle Obama',
+      description:
+        'The memoir of the former First Lady of the United States, sharing her journey from childhood to the White House.',
+      image: 'Becoming.jpg',
+      category: categories[1]._id,
+      price: 19.99,
+      qauntity: 500
+    },
+    {
+      name: 'Sapiens: A Brief History of Humankind',
+      description:
+        'A thought-provoking exploration of the history of Homo sapiens and the forces that have shaped our species.',
+      image: 'sapiens.jpg',
+      category: categories[1]._id,
+      price: 19.99,
+      qauntity: 500
+    },
+    {
+      name: 'The Great Gatsby by F. Scott Fitzgerald',
+      description:
+        'The Great Gatsby" follows the enigmatic Jay Gatsby and his pursuit of the American Dream.',
+      image: 'gatsby.jpg',
+      category: categories[1]._id,
+      price: 19.99,
+      qauntity: 500
+    },
+    {
+      name: 'To Kill a Mockingbird" by Harper Lee',
+      description:
+        'To Kill a Mockingbird explores themes of racial injustice, moral growth, and compassion in the Deep South during the 1930s.',
+      image: 'mockingbird.jpg',
+      category: categories[1]._id,
+      price: 19.99,
+      qauntity: 500
+    },
+    {
+      name: 'Dune by Frank Herbert',
+      description:
+        'Dune is a timeless science fiction masterpiece that transports readers to the desert planet of Arrakis, where noble houses vie for control of the most valuable substance in the universe: spice.',
+      image: 'dune.jpg',
+      category: categories[1]._id,
+      price: 19.99,
+      qauntity: 500
+    },
+
+
+
+
+
+
+
+
 
 
 
