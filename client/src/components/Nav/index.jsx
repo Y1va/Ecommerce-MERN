6ce319bb7138d7f361c24c845143057b5533a64a
Bjorn-Ironsidee <109214871+Bjorn-Ironsidee@users.noreply.github.com>
2024-05-06 19:31:14 +1000

    nav comments
    nav comments

--- client/src/components/Nav/index.jsx
+++ client/src/components/Nav/index.jsx
@@ -1,10 +1,15 @@
+// Importing Auth from the auth utility
 import Auth from "../../utils/auth";
+// Importing Link from react-router-dom for client-side navigation
 import { Link } from "react-router-dom";
 
+// Nav component for rendering the navigation bar
 function Nav() {
 
+  // Function to render navigation links based on user authentication status
   function showNavigation() {
     if (Auth.loggedIn()) {
+      // If user is logged in, render links for Order History and Logout
       return (
         <ul className="flex-row">
           <li className="mx-1">
@@ -13,7 +18,7 @@ function Nav() {
             </Link>
           </li>
           <li className="mx-1">
-            {/* this is not using the Link component to logout or user and then refresh the application to the start */}
+            {/* Logout link using an anchor tag with onClick event */}
             <a href="/" onClick={() => Auth.logout()}>
               Logout
             </a>
@@ -21,6 +26,7 @@ function Nav() {
         </ul>
       );
     } else {
+      // If user is not logged in, render links for Signup and Login
       return (
         <ul className="flex-row">
           <li className="mx-1">
@@ -38,15 +44,18 @@ function Nav() {
     }
   }
 
+  // Rendering the navigation bar
   return (
     <header className="flex-row px-1">
       <h1>
+        {/* Logo with a link to the home page */}
         <Link to="/">
           <span role="img" aria-label="shopping bag">🛍️</span>
           -Shop-Shop
         </Link>
       </h1>
 
+      {/* Render the navigation links */}
       <nav>
         {showNavigation()}
       </nav>
@@ -54,4 +63,5 @@ function Nav() {
   );
 }
 
+// Exporting the Nav component
 export default Nav;
