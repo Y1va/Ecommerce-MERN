096872cb51c32d28868115a886d1827ef34aaab1
ZeuS <ponzeadrian@gmail.com>
2024-05-06 19:43:09 +1000

    added comments

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,14 +1,16 @@
+// Import necessary components from local files
 import ProductList from '../components/ProductList';
 import CategoryMenu from "../components/CategoryMenu";
 import Cart from '../components/Cart';
 
-
+// Define the Home component
 const Home = () => {
+  // Return a JSX element that includes the CategoryMenu, ProductList, and Cart components
   return (
     <div className='container'>
-      <CategoryMenu />
-      <ProductList />
-      <Cart />
+      <CategoryMenu /> {/* This is where the category menu will be rendered */}
+      <ProductList /> {/* This is where the list of products will be rendered */}
+      <Cart /> {/* This is where the shopping cart will be rendered */}
     </div>
   );
 };
